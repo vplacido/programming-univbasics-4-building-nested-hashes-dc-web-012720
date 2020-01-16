998f9ed01d69a67b6f568e3469b269b9b9ad4481
Valentin Placido <valentinplacido25@gmail.com>
2020-01-16 18:00:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = base_hash
-  monopoly[:railroads] = {pieces: 4}
+  monopoly = {railroads: {}, pieces: 4}
   monopoly
 end
 
@@ -14,9 +13,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly = monopoly_with_second_tier
-  monopoly[:railroads] => {names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}}
-  monopoly[:railroads] = {rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}}
+  monopoly = {railroads: {}, pieces: 4, names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}}
   monopoly
 end
 
