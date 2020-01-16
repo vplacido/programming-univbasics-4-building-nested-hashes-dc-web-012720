09bf79b30a8d9fc647aee604879fc1321ab6cc44
Valentin Placido <valentinplacido25@gmail.com>
2020-01-16 17:43:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly = base_hash
-  monopoly[:railroads] = {pieces: 4
+  monopoly[:railroads] = {pieces: 4}
   monopoly
 end
 
