fbc34b4b3a007d7f104824b59cb37201f3f4ea5d
Valentin Placido <valentinplacido25@gmail.com>
2020-01-16 17:56:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_third_tier
   # complexity.
   monopoly = monopoly_with_second_tier
   monopoly[:railroads] = {names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}}
-  monopoly[:railroads] = {rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
+  monopoly[:railroads] = {rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}}
   monopoly
 end
 
